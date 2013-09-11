a8cb0aaba330a9bd3db192d091997448d965a751
jon-a-nygaard <jon.arild@highsoft.com>
2013-09-11 09:53:51 +0200

    Added manifest and plugin file
    - Added the manifest file which includes describing data.
    - Added the plugin file containing the code.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 draggable-points
 ================
+This plugin allows the user to drag the points in the chart, making them able to edit data directly in the chart.
+
+The contents of the plugin is located in the javascript file "draggable-points.js". 
+This plugin is published under the MIT license, and the license document is included in the repository.
---
+++ draggable-points.js
@@ -0,0 +1,149 @@
+/**
+ * Experimental Draggable points plugin
+ * Revised 2013-06-13
+ * Author: Torstein Hønsi
+ * License: MIT License
+ *
+ */
+(function (Highcharts) {
+    var addEvent = Highcharts.addEvent,
+        each = Highcharts.each;
+
+    /**
+     * Filter by dragMin and dragMax
+     */
+    function filterRange(newY, series, XOrY) {
+        var options = series.options,
+            dragMin = options['dragMin' + XOrY],
+            dragMax = options['dragMax' + XOrY];
+
+        if (newY < dragMin) {
+            newY = dragMin;
+        } else if (newY > dragMax) {
+            newY = dragMax;
+        }
+        return newY;
+    }
+
+    Highcharts.Chart.prototype.callbacks.push(function (chart) {
+
+        var container = chart.container,
+            dragPoint,
+            dragX,
+            dragY,
+            dragPlotX,
+            dragPlotY;
+
+        chart.redraw(); // kill animation (why was this again?)
+
+        addEvent(container, 'mousedown', function (e) {
+            var hoverPoint = chart.hoverPoint,
+                options;
+
+            if (hoverPoint) {
+                options = hoverPoint.series.options;
+                if (options.draggableX) {
+                    dragPoint = hoverPoint;
+
+                    dragX = e.pageX;
+                    dragPlotX = dragPoint.plotX;
+                }
+
+                if (options.draggableY) {
+                    dragPoint = hoverPoint;
+
+                    dragY = e.pageY;
+                    dragPlotY = dragPoint.plotY + (chart.plotHeight - (dragPoint.yBottom || chart.plotHeight));
+                }
+
+                // Disable zooming when dragging
+                if (dragPoint) {
+                    chart.mouseIsDown = false;
+                }
+            }
+        });
+
+        addEvent(container, 'mousemove', function (e) {
+            if (dragPoint) {
+                var deltaY = dragY - e.pageY,
+                    deltaX = dragX - e.pageX,
+                    newPlotX = dragPlotX - deltaX - dragPoint.series.xAxis.minPixelPadding,
+                    newPlotY = chart.plotHeight - dragPlotY + deltaY,
+                    newX = dragX === undefined ? dragPoint.x : dragPoint.series.xAxis.translate(newPlotX, true),
+                    newY = dragY === undefined ? dragPoint.y : dragPoint.series.yAxis.translate(newPlotY, true),
+                    series = dragPoint.series,
+                    proceed;
+
+                newX = filterRange(newX, series, 'X');
+                newY = filterRange(newY, series, 'Y');
+
+                // Fire the 'drag' event with a default action to move the point.
+                dragPoint.firePointEvent(
+                    'drag', {
+                    newX: newX,
+                    newY: newY
+                },
+
+                function () {
+                    proceed = true;
+                    dragPoint.update([newX, newY], false);
+                    chart.tooltip.refresh(chart.tooltip.shared ? [dragPoint] : dragPoint);
+                    if (series.stackKey) {
+                        chart.redraw();
+                    } else {
+                        series.redraw();
+                    }
+                });
+                
+                // The default handler has not run because of prevented default
+                if (!proceed) {
+                    drop();
+                }
+            }
+        });
+
+        function drop(e) {
+            if (dragPoint) {
+                if (e) {
+                    var deltaX = dragX - e.pageX,
+                        deltaY = dragY - e.pageY,
+                        newPlotX = dragPlotX - deltaX - dragPoint.series.xAxis.minPixelPadding,
+                        newPlotY = chart.plotHeight - dragPlotY + deltaY,
+                        series = dragPoint.series,
+                        newX = dragX === undefined ? dragPoint.x : dragPoint.series.xAxis.translate(newPlotX, true),
+                        newY = dragY === undefined ? dragPoint.y : dragPoint.series.yAxis.translate(newPlotY, true);
+    
+                    newX = filterRange(newX, series, 'X');
+                    newY = filterRange(newY, series, 'Y');
+                    dragPoint.update([newX, newY]);
+                }                
+                dragPoint.firePointEvent('drop');
+            }
+            dragPoint = dragX = dragY = undefined;
+        }
+        addEvent(document, 'mouseup', drop);
+        addEvent(container, 'mouseleave', drop);
+    });
+
+    /**
+     * Extend the column chart tracker by visualizing the tracker object for small points
+     */
+    var colProto = Highcharts.seriesTypes.column.prototype,
+        baseDrawTracker = colProto.drawTracker;
+
+    colProto.drawTracker = function () {
+        var series = this;
+        baseDrawTracker.apply(series);
+        each(series.points, function (point) {
+            point.graphic.attr(point.shapeArgs.height < 3 ? {
+                'stroke': 'black',
+                'stroke-width': 2,
+                'dashstyle': 'shortdot'
+            } : {
+                'stroke-width': series.options.borderWidth,
+                'dashstyle': series.options.dashStyle || 'solid'
+            });
+        });
+    };
+ 
+})(Highcharts);
