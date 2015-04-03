5f91b32ed0437b3ce7952ec70356c90313158099
bozdoz <howaboutben@gmail.com>
2015-04-03 14:42:03 -0600

    added inputs to draggable charts if user adds dragInputs:true in their series; Reason being dragging can be very inaccurate;

--- draggable-points.js
+++ draggable-points.js
@@ -7,6 +7,7 @@
  (function (Highcharts) {
 
         var addEvent = Highcharts.addEvent,
+            removeEvent = Highcharts.removeEvent,
             each = Highcharts.each,
             pick = Highcharts.pick;
 
@@ -100,23 +101,26 @@
                         }, function () {
                             proceed = true;
 
-                            dragPoint.update({
-                                x: draggableX ? newX : dragPoint.x,
-                                y: draggableY ? newY : dragPoint.y
-                            }, false);
+                            if (newY != dragPoint.y || newX != dragPoint.x) {
 
-                            // Hide halo while dragging (#14)
-                            if (series.halo) {
-                                series.halo = series.halo.destroy();
-                            }
+                                dragPoint.update({
+                                    x: draggableX ? newX : dragPoint.x,
+                                    y: draggableY ? newY : dragPoint.y
+                                }, false);
 
-                            if (chart.tooltip) {
-                                chart.tooltip.refresh(chart.tooltip.shared ? [dragPoint] : dragPoint);
-                            }
-                            if (series.stackKey) {
-                                chart.redraw();
-                            } else {
-                                series.redraw();
+                                // Hide halo while dragging (#14)
+                                if (series.halo) {
+                                    series.halo = series.halo.destroy();
+                                }
+
+                                if (chart.tooltip) {
+                                    chart.tooltip.refresh(chart.tooltip.shared ? [dragPoint] : dragPoint);
+                                }
+                                if (series.stackKey) {
+                                    chart.redraw();
+                                } else {
+                                    series.redraw();
+                                }
                             }
                         }
                     );
@@ -154,10 +158,12 @@
                             newY = Number(Highcharts.numberFormat(newY, decimals));
                         }
 
-                        dragPoint.update({
-                            x: draggableX ? newX : dragPoint.x,
-                            y: draggableY ? newY : dragPoint.y
-                        });
+                        if (newY != dragPoint.y || newX != dragPoint.x) {
+                            dragPoint.update({
+                                x: draggableX ? newX : dragPoint.x,
+                                y: draggableY ? newY : dragPoint.y
+                            });
+                        }
                     }
                     dragPoint.firePointEvent('drop');
                 }
@@ -167,7 +173,95 @@
             // Kill animation (why was this again?)
             chart.redraw(); 
 
+            function addInputsToCharts() {
+                for (var s = 0, slen = chart.series.length; s < slen; s++) {
+                    var series = chart.series[s],
+                        options = series.options,
+                        points = series.points;
+                    if ( options.dragInputs ) {
+                        for (var p = 0, plen = points.length; p < plen; p++) {
+                            var point = points[p];
+                            addInput(point);                        
+                        }
+                    }
+                }
+
+
+                function addInput(point) {
+                    var input = document.createElement('input'),
+                        is_keyup = false;
+
+                    container.parentNode.appendChild(input);
+
+                    input.value = point.y;
+                    input.style.position = 'absolute';
+                    input.style.visibility = 'hidden';
+                    input.style.textAlign = 'center';
+                    input.className = 'draggable-input-value';
+                    input.style.width = point.pointWidth + 'px';
+                    input.style.left = (chart.plotLeft + point.plotX - input.offsetWidth/2) + 'px';
+                    //input.style.top = (point.plotY) + 'px';
+                    input.style.top = (chart.plotTop - input.offsetHeight/2) + 'px';
+
+                    addEvent(input, 'mouseenter', function (event) {
+                        var width = Number(this.style.width.slice(0, this.style.width.length -2)),
+                            left = Number(this.style.left.slice(0, this.style.left.length -2));
+                        this.style.visibility = 'visible'; 
+                        this.style.width = (width*2) + 'px';
+                        this.style.left = (left - (width/2)) + 'px';
+                        addEvent(input, 'mouseleave', function (event) {
+                            this.style.width = width + 'px';
+                            this.style.left = left + 'px';
+                            removeEvent(event);
+                        });
+                    });
+
+                    addEvent(input, 'focus', function (event) {
+                        this.select();
+                    });
+
+                    addEvent(input, 'click', function (event) {
+                        setTimeout(function () {
+                            input.select();
+                        }, 5);
+                    });
+
+                    addEvent(input, 'keyup', function (event) {
+                        is_keyup = true;
+                        point.update({
+                            x : point.x,
+                            y : Number(this.value),
+                        });
+                        is_keyup = false;
+                    });
+
+                    addEvent(point, 'update', function (event) {
+                        if (!is_keyup) {
+                            input.value = event.options.y;
+                        }
+                    });
+
+                    addEvent(point, 'click', function (event) {
+                        input.focus();
+                    });
+                }
+            }
+
+            function inputDisplay (display_type) {
+                return function () {
+                    var inputs = container.parentNode.getElementsByClassName('draggable-input-value');
+                    for (var i = 0, len = inputs.length; i < len; i++) {
+                        var input = inputs[i];
+                        input.style.visibility = display_type;
+                    }
+                }
+            }
+
+            addInputsToCharts();
+
             // Add'em
+            addEvent(container, 'mouseenter', inputDisplay('visible'));
+            addEvent(container, 'mouseleave', inputDisplay('hidden'));
             addEvent(container, 'mousemove', mouseMove);
             addEvent(container, 'touchmove', mouseMove);
             addEvent(container, 'mousedown', mouseDown);
